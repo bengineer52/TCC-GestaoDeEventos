965adf2a1327b84b8cfbaa108822d8b71eabe699
Bernardo Barreto <bernardo.barreto@housecallpro.com>
2024-10-26 11:43:00 -0300

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,34 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+[
+  Subscription,
+  Event,
+  User
+].each(&:delete_all)
+
+password = 'password1234'
+
+user_1 = User.create!(email: 'user1@email.com', password: password, password_confirmation: password)
+user_2 = User.create!(email: 'user2@email.com', password: password, password_confirmation: password)
+
+event_1 = Event.create!(
+  user: user_1,
+  name: 'Django introduction',
+  schedule: 2.week.from_now,
+  location: '24 main street, NY'
+)
+
+Subscription.create!(
+  user: user_2,
+  event: event_1
+)
+
+event_2 = Event.create!(
+  user: user_2,
+  name: 'RubyOnRails introduction',
+  schedule: 1.week.from_now,
+  location: '22 main street, NY'
+)
+
+Subscription.create!(
+  user: user_1,
+  event: event_2
+)
