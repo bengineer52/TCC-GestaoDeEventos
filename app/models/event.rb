cb811fb524266bcbb2b537e5e547150442b98e8c
Bernardo Barreto <bernardo.barreto@housecallpro.com>
2024-10-26 16:23:40 -0300

    add event#find_subscription(user) helper

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,4 +9,8 @@ class Event < ApplicationRecord
   def subscribed?(param_user)
     subscriptions.exists?(user_id: param_user.id)
   end
+
+  def find_subscription(param_user)
+    subscriptions.find_by(user_id: param_user)
+  end
 end
